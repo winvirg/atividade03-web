922515b8b970e681c239c6f3dd211b4a4e3e485d
winvirg <thiagowinvirg@gmail.com>
2021-10-26 18:20:56 -0300

    Mostrando mais votados, obtendo menor voto, problemas ao mostrar os menos votados

--- src/components/Votacao.jsx
+++ src/components/Votacao.jsx
@@ -7,20 +7,41 @@ export default function Votacao(){
     const [countJuazeiro, setCountJuazeiro] = useState(0);
     const [moreVoted, setMoreVoted] = useState([""]);
     const [moreNumberVotes, setMoreNumberVotes] = useState(0);
+    const [lassVoted, setLassVoted] = useState([""]);
+    const [lassNumberVotes, setLassNumberVotes] = useState();
 
-    function comparar(city){
+    function lassCompare(cityList){
+        let menor = cityList[0].count;
+
+        for (let i = 0; i < cityList.length; i++) {
+            for (let j = i+1; j < cityList.length; j++) {
+                if (cityList[j].count < menor) {
+                    menor = cityList[j].count;
+                }
+            }
+        }
+        
+        for (let i = 0; i < cityList.length; i++) {
+            if(cityList[i].count === menor ){
+                setLassVoted([...lassVoted, cityList[i].name])
+            }
+        }
+    }
+
+    function compare(city){
         switch (city) {
             case "Fortaleza":
                 setCountFortaleza(countFortaleza + 1);
                 let localFortaleza = countFortaleza +1;
 
                 if(localFortaleza===moreNumberVotes){
-                    setMoreVoted([...moreVoted, "Fortaleza"])
+                    setMoreVoted([...moreVoted, ", Fortaleza"])
                 }
                 else if(localFortaleza>moreNumberVotes){
                     setMoreVoted(["Fortaleza"]);
                     setMoreNumberVotes(localFortaleza);
                 }
+                lassCompare([{name:"Fortaleza", count:localFortaleza}, {name:"Quixadá", count:countQuixada}, {name:"Limoeiro", count:countLimoeiro}, {name:"Juazeiro", count:countJuazeiro}])
                 break;
 
             case "Quixadá":
@@ -28,12 +49,41 @@ export default function Votacao(){
                 let localQuixada = countQuixada +1;
 
                 if(localQuixada===moreNumberVotes){
-                    setMoreVoted([...moreVoted, "Quixadá"])
+                    setMoreVoted([...moreVoted, ", Quixadá"])
                 }
                 else if(localQuixada>moreNumberVotes){
                     setMoreVoted(["Quixadá"]);
                     setMoreNumberVotes(localQuixada);
                 }
+                lassCompare([{name:"Fortaleza", count:countFortaleza}, {name:"Quixadá", count:localQuixada}, {name:"Limoeiro", count:countLimoeiro}, {name:"Juazeiro", count:countJuazeiro}])
+                break;
+
+            case "Limoeiro":
+                setCountLimoeiro(countLimoeiro + 1);
+                let localLimoeiro = countLimoeiro +1;
+
+                if(localLimoeiro===moreNumberVotes){
+                    setMoreVoted([...moreVoted, ", Limoeiro"])
+                }
+                else if(localLimoeiro>moreNumberVotes){
+                    setMoreVoted(["Limoeiro"]);
+                    setMoreNumberVotes(localLimoeiro);
+                }
+                lassCompare([{name:"Fortaleza", count:countFortaleza}, {name:"Quixadá", count:countQuixada}, {name:"Limoeiro", count:localLimoeiro}, {name:"Juazeiro", count:countJuazeiro}])
+                break;
+
+            case "Juazeiro":
+                setCountJuazeiro(countJuazeiro + 1);
+                let localJuazeiro = countJuazeiro +1;
+
+                if(localJuazeiro===moreNumberVotes){
+                    setMoreVoted([...moreVoted, ", Juazeiro"])
+                }
+                else if(localJuazeiro>moreNumberVotes){
+                    setMoreVoted(["Juazeiro"]);
+                    setMoreNumberVotes(localJuazeiro);
+                }
+                lassCompare([{name:"Fortaleza", count:countFortaleza}, {name:"Quixadá", count:countQuixada}, {name:"Limoeiro", count:countLimoeiro}, {name:"Juazeiro", count:localJuazeiro}])
                 break;
 
             default:
@@ -43,10 +93,10 @@ export default function Votacao(){
     return (
         <div>
             <h1>VOTAÇÃO DE CIDADES</h1>
-            <button onClick={() => comparar("Fortaleza")} color='red'>Fortaleza</button>
-            <button onClick={() => comparar("Quixadá")} color='blue'>Quixadá</button>
-            <button onClick={() => setCountLimoeiro(countLimoeiro + 1)} color='green'>Limoeiro</button>
-            <button onClick={() => setCountJuazeiro(countJuazeiro + 1)} color='yellow'>Juazeiro</button>
+            <button onClick={() => compare("Fortaleza")} color='red'>Fortaleza</button>
+            <button onClick={() => compare("Quixadá")} color='blue'>Quixadá</button>
+            <button onClick={() => compare("Limoeiro")} color='green'>Limoeiro</button>
+            <button onClick={() => compare("Juazeiro")} color='yellow'>Juazeiro</button>
             
             <p>Fortaleza: {countFortaleza}</p>
             <p>Quixada: {countQuixada}</p>
@@ -54,7 +104,7 @@ export default function Votacao(){
             <p>Juazeiro: {countJuazeiro}</p>
 
             <p>Cidades mais votadas:{moreVoted}</p>
-        {console.log("out -> \n", {fortalez:countFortaleza, qixada:countQuixada, maior:moreNumberVotes, citys:moreVoted})}
+            <p>Cidades menos votadas:{lassVoted}</p>
         </div>
     )
 }
